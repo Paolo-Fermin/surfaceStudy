eface7ecc35dbe67366652ca33d0e6f933c03beb
Paolo Fermin <paolofermin@vt.edu>
2019-07-17 11:44:51 -0400

    completed __getitem__ function to return input data and output tensor

--- torch_dataset.py
+++ torch_dataset.py
@@ -27,36 +27,38 @@ class WakeDataset(Dataset):
 	
 	'''
 		self.root_dir = root_dir
+		self.num_samples = sum(os.path.isdir(i) for i in os.listdir(self.root_dir))
+		
+		self.input_combos = []
+		#get input variable combinations and store them in a list
+		for i, case in enumerate(filter(os.path.isdir, os.listdir(os.getcwd()))):
+			self.input_combos.append([float(case[4:9]), float(case[-3:])])
+	
+		#assign an index to each dTdz, d0 combination	
+		#for i in range(self.num_samples):
+
+		#turn input combos into a tensor
+		#input_combos_tensor = torch.FloatTensor(input_combos)
+			
 		self.transform = transform
 
 	def __len__(self):
-		#return 2 times the number of directories in the root directory 
-		return 2 * sum(os.path.isdir(i) for i in os.listdir(self.root_dir))
+		return self.num_samples
 
-	def __getitem__(self, dTdz, d0):
+	def __getitem__(self, index):
 		#return data in necessary format
 		
 		#create array that will store 
 		#(dTdz, d0, [UySym_down], [UzSym_down])
-		case_dirs = [f for f in os.listdir(root_dir) if f.startswith('dTdz')
-		for case in case_dirs:
-			self.
-		
-		return self.wake_image
+		case_dir = '/%s/dTdz%0.3f_z%d' % (root_dir, self.input_combos[index][0], self.input_combos[index][1])
+		uy_data = pd.read_csv(os.path.join(case_dir, 'UySym_down.csv'), index_col=0)
+		self.uy_data_tensor = torch.FloatTensor(uy_data.values)
+		#print(uy_data_tensor)
+		#uz_data = pd.read_csv(os.path.join(case_dir, 'UzSym_down.csv'), index_col=0)
+		#uz_data_tensor = torch.FloatTensor(uz_data.values)
+		#print(uz_data_tensor)
 
+		#self.images = torch.FloatTensor(uy_data.values, uz_data.values)		
 	
-	#function to downsample data to be 128x256
-	def downsample(self, dire, component_file):
-		
-		#recombine file with directory path 
-		component_filepath = os.path.join(dire, component_file)
-		print(component_filepath)
-		results = pd.read_csv(component_filepath)
-		component_name = os.path.splitext(component_filepath)
-		print(component_name)
+		return self.input_combos, self.uy_data_tensor
 
-		results_down = results.iloc[:, ::8] #get every nth value 
-		results_down.drop(results_down.columns[-9:], axis=1, inplace=True) #drop last 9 columns to get even 256
-		results_down.to_csv('%s_down.csv' % component_name[0])
-			
-		
