c8659e31c634cec74fd5eccfbbe867dbe9676a19
Paolo Fermin <paolofermin@vt.edu>
2019-07-18 14:15:27 -0400

    added data trimming and fixed architecture (again) to 128 x 1024

--- torch_nn.py
+++ torch_nn.py
@@ -21,7 +21,7 @@ import visdom
 torch.manual_seed(8)
 
 model = nn.Sequential(
-	nn.ConvTranspose2d(1, 256, kernel_size=(2, 7), stride=1, padding=0),
+	nn.ConvTranspose2d(1, 256, kernel_size=(1, 7), stride=1, padding=0),
 	nn.PReLU(),
 	nn.InstanceNorm2d(1),
 	nn.ConvTranspose2d(256, 128, kernel_size=(4, 4), stride=2, padding=1),
@@ -34,7 +34,9 @@ model = nn.Sequential(
 	nn.PReLU(),	
 	nn.ConvTranspose2d(16, 8, kernel_size=(4, 4), stride=2, padding=1),
 	nn.PReLU(),
-	nn.ConvTranspose2d(8, 1, kernel_size=(4, 4), stride=2, padding=1),		
+	nn.ConvTranspose2d(8, 4, kernel_size=(4, 4), stride=2, padding=1),	
+	nn.PReLU(),		
+	nn.ConvTranspose2d(4, 1, kernel_size=(4, 4), stride=2, padding=1),
 	nn.Tanh()
 )
 
@@ -66,10 +68,20 @@ class WakeDataset(Dataset):
 	def __getitem__(self, index):
 		#return data in necessary format
 		case_dir = '/%s/dTdz%0.3f_z%d' % (self.root_dir, self.input_combos_tensor[index][0], self.input_combos_tensor[index][1])
-		uy_data = pd.read_csv(os.path.join(case_dir, 'UySym_down.csv'), index_col=0)
-		self.uy_data_tensor = torch.FloatTensor(uy_data.values)
+		uy_data = pd.read_csv(os.path.join(case_dir, 'Uy.csv'), header=None)
+		#print(uy_data)		
+		
+		#trim data to 128x1024		
+		drop_cols = 33
+		uy_data.drop(uy_data.columns[:drop_cols/2], axis=1, inplace=True)
+		uy_data.drop(uy_data.columns[-drop_cols/2-1:], axis=1, inplace=True)
+		#print(uy_data)
 
-		return self.input_combos_tensor[index].view(1, 1, 2), self.uy_data_tensor.view(1, 1, 128, 256
+		#convert data to tensor
+		self.uy_data_tensor = torch.FloatTensor(uy_data.values)
+		#print('Target size: ' + str(self.uy_data_tensor.size()))
+
+		return self.input_combos_tensor[index].view(1, 1, 2), self.uy_data_tensor.view(1, 128, 1024)
 
 print('cwd: ' + str(os.getcwd()))
 
@@ -84,14 +96,14 @@ val_loader = DataLoader(dataset=val_dataset, batch_size=1, shuffle=True)
 
 loss_fn = nn.MSELoss()
 
-lr = 1e-5
+lr = 1e-3
 optimizer = optim.Adam(model.parameters(), lr=lr)
 epochs = 2000
 log_interval = 100
 
 #create trainer and evaluator
 trainer = create_supervised_trainer(model, optimizer, loss_fn)
-evaluator = create_supervised_evaluator(model, metrics={'nll':Loss(loss_fn)})
+evaluator = create_supervised_evaluator(model, metrics={'mse':Loss(loss_fn)})
 
 #create visdom plots
 vis = visdom.Visdom()
@@ -113,27 +125,29 @@ def log_training_loss(engine):
 			 update='append', win=train_loss_window)
 
 @trainer.on(Events.EPOCH_COMPLETED)
-def log_training_results(trainer): 
+def log_training_results(engine): 
 	evaluator.run(train_loader)
-	metric = evaluator.state.metrics
-	avg_nll = metrics['nll']
-	print('Training Results - Epoch: {} Avg Loss: {:.6e}'.format(engine.state.epoch, avg_nll))
-	vis.line(X=np.array([engine.state.epoch]), Y=np.array([avg_nll]), 
+	metrics = evaluator.state.metrics
+	avg_mse = metrics['mse']
+	print('Training - Epoch: {} Avg Loss: {:.6e}'.format(engine.state.epoch, avg_mse))
+	vis.line(X=np.array([engine.state.epoch]), Y=np.array([avg_mse]), 
 		win=train_avg_loss_window, update='append')
 
 @trainer.on(Events.EPOCH_COMPLETED)
-def log_validation_results(trainer):
+def log_validation_results(engine):
 	evaluator.run(val_loader)
 	metrics = evaluator.state.metrics
-	avg_nll = metrics['nll']
-	print('Validation Results - Epoch: {} Avg Loss: {:.6e}'.format(trainer.state.epoch, 			avg_nll))
-	vis.line(X=np.array([engine.state.epoch]), Y=np.array([avg_nll]), 
+	avg_mse = metrics['mse']
+	print('Validation - Epoch: {} Avg Loss: {:.6e}\n'.format(engine.state.epoch, avg_mse))
+	vis.line(X=np.array([engine.state.epoch]), Y=np.array([avg_mse]), 
 		win=val_avg_loss_window, update='append')
 
 #run training
 trainer.run(train_loader, max_epochs=epochs)
 
-		
+print('Saving model...')
+torch.save(model.state_dict(), 'wake_net.pt')
+print('Model saved')
 print('Finished training')
 print('Execution time: ' + str(datetime.now() - start_time))
 
