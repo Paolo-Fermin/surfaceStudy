cde98a881437a9c2407fecdfbf1f9888cef3eb68
Paolo Fermin <paolofermin@vt.edu>
2019-07-23 11:03:27 -0400

    quadrupled the channel size

--- wake_model.py
+++ wake_model.py
@@ -32,28 +32,28 @@ class WakeModel(nn.Module):
 def WakeModel():
 	
 	model = nn.Sequential(
-		nn.ConvTranspose2d(1, 256, kernel_size=(1, 7), stride=1, padding=0),
+		nn.ConvTranspose2d(1, 1024, kernel_size=(1, 7), stride=1, padding=0),
 		nn.PReLU(),
 		nn.InstanceNorm2d(1),
+		nn.ConvTranspose2d(1024, 512, kernel_size=(4, 4), stride=2, padding=1),
+		nn.PReLU(),
+		nn.Dropout(p=0.1), 
+		nn.ConvTranspose2d(512, 256, kernel_size=(4, 4), stride=2, padding=1),
+		nn.PReLU(),
+		nn.Dropout(p=0.1), 
 		nn.ConvTranspose2d(256, 128, kernel_size=(4, 4), stride=2, padding=1),
 		nn.PReLU(),
 		nn.Dropout(p=0.1), 
 		nn.ConvTranspose2d(128, 64, kernel_size=(4, 4), stride=2, padding=1),
-		nn.PReLU(),
+		nn.PReLU(),	
 		nn.Dropout(p=0.1), 
 		nn.ConvTranspose2d(64, 32, kernel_size=(4, 4), stride=2, padding=1),
 		nn.PReLU(),
 		nn.Dropout(p=0.1), 
-		nn.ConvTranspose2d(32, 16, kernel_size=(4, 4), stride=2, padding=1),
-		nn.PReLU(),	
-		nn.Dropout(p=0.1), 
-		nn.ConvTranspose2d(16, 8, kernel_size=(4, 4), stride=2, padding=1),
-		nn.PReLU(),
-		nn.Dropout(p=0.1), 
-		nn.ConvTranspose2d(8, 4, kernel_size=(4, 4), stride=2, padding=1),	
+		nn.ConvTranspose2d(32, 16, kernel_size=(4, 4), stride=2, padding=1),	
 		nn.PReLU(),	
 		nn.Dropout(p=0.1), 	
-		nn.ConvTranspose2d(4, 1, kernel_size=(4, 4), stride=2, padding=1),
+		nn.ConvTranspose2d(16, 1, kernel_size=(4, 4), stride=2, padding=1),
 		nn.Tanh()
 	)
 	
